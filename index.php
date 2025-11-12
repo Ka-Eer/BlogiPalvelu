bd9769b46b2f54985cc1de582959b30f96bc8292
Sadbrew <132399464+Sadbrew@users.noreply.github.com>
2025-11-12 11:26:24 +0200

    Create index.php
    index.php tuomaan html sivut palvelimelle

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+// php tuo html sivut palvelimelle
+
+// XAMPP Apache palvelin
+// C:\xampp\htdocs kansion sisältö korvattu GitHub repo sisällöllä
+
+$pages = [
+    '' => 'index.html',
+    'index' => 'index.html',
+    'blogi' => 'blogi.html',
+    'galleria' => 'galleria.html',
+];
+
+$req = isset($_GET['page']) ? (string) $_GET['page'] : '';
+
+if (array_key_exists($req, $pages)) {
+    $file = __DIR__ . DIRECTORY_SEPARATOR . $pages[$req];
+    if (is_readable($file)) {
+        header('Content-Type: text/html; charset=utf-8');
+        // välttää tiedoston lataaminen kokonaan muistiin ennen lähettämistä
+        readfile($file);
+        exit;
+    } else {
+        http_response_code(404);
+        echo "<h1>404 Not Found</h1><p>File not found: " . htmlspecialchars($pages[$req]) . "</p>";
+        exit;
+    }
+}
