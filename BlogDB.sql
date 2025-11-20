06d548c948b25ba6af996b23393ed4f8cf3eaf30
Ka-Eer <eero.karjalainen@edu.hyria.fi>
2025-11-20 09:58:26 +0200

    Checkboxit blogitageille + sql upd
    ei vielä lähetä mitään

--- BlogDB.sql
+++ BlogDB.sql
@@ -11,5 +11,17 @@ CREATE TABLE `blogit` (
   `Teksti` text NOT NULL,
   `Kuva` mediumblob DEFAULT NULL,
   `Tykkaykset` int DEFAULT 0,
+  /* BlogTag tagit true/false */
+  `BT1` boolean NOT NULL DEFAULT 0,
+  `BT2` boolean NOT NULL DEFAULT 0,
+  `BT3` boolean NOT NULL DEFAULT 0,
+  `BT4` boolean NOT NULL DEFAULT 0,
+  `BT5` boolean NOT NULL DEFAULT 0,
+  `BT6` boolean NOT NULL DEFAULT 0,
+  `BT7` boolean NOT NULL DEFAULT 0,
+  `BT8` boolean NOT NULL DEFAULT 0,
+  `BT9` boolean NOT NULL DEFAULT 0,
+  `BT10` boolean NOT NULL DEFAULT 0,
+  `BT11` boolean NOT NULL DEFAULT 0,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
