49a29d0ed8be96d07a597a22d4acca1562903210
Joona <joona.tihekari@gmail.com>
2025-12-01 10:47:28 +0200

    Esimerkki taulu testaamiseen
    Esimerkki taulu tykkäystoiminnon ja session simuloimiseen.

---
+++ esimerkkikayttajat.sql
@@ -0,0 +1,14 @@
+-- Esimerkkikäyttäjät blogipalvelun users-tauluun
+-- Voit importata tämän tiedoston MySQL:ään
+
+INSERT INTO users (kayttajaNimi, salasana) VALUES
+('maija', 'salasana123'),
+('kalle', 'kallepassu'),
+('liisa', 'liisankoodi'),
+('janne', 'janne1234'),
+('sofia', 'sofiapass'),
+('mikko', 'mikkosala'),
+('emma', 'emmaemma'),
+('teemu', 'teemunpassu'),
+('sara', 'salasara'),
+('ville', 'villekoodi');
